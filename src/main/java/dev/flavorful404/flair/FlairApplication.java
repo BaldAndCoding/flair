2ef6d3f0e926040eb2de27feb0198d5488b60095
BaldAndCoding <baldandcoding@gmail.com>
2024-01-12 18:44:18 -0500

    Initial SpringBoot app

---
+++ src/main/java/dev/flavorful404/flair/FlairApplication.java
@@ -0,0 +1,13 @@
+package dev.flavorful404.flair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/flavorful404/flair/FlairApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.flavorful404.flair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
